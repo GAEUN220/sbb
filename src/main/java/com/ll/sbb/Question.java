d39e5b729a533a7ca0277677afcde8369fd1fac9
gaeun220 <gaeun2701@gmail.com>
2024-12-11 17:07:05 +0900

    Entity

---
+++ src/main/java/com/ll/sbb/Question.java
@@ -0,0 +1,28 @@
+package com.ll.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 해당 속성만 별도로 번호가 차례때로 늘어나도록 할 때 사용
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
