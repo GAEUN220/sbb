e411541967c808f7fb81b6094335fadea4d50ecc
gaeun220 <gaeun2701@gmail.com>
2024-12-12 15:11:08 +0900

    rootURL

--- src/main/java/com/ll/sbb/MainController.java
+++ src/main/java/com/ll/sbb/MainController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    @GetMapping
+    @GetMapping("/sbb")
     @ResponseBody // URL 요청에 대한 응답 리턴
     public String index() {
         return "안녕하세요 sbb에 오신 것을 환영합니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
